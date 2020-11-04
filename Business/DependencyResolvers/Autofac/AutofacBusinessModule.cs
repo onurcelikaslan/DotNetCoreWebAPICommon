9d8e9dcdded5858396db8cb92d2a500752ec9a97
Onur Celikaslan <onurcelikaslan@gmail.com>
2020-11-04 17:37:32 +0000

    project first commit

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //builder.RegisterType<ProductManager>().As<IProductService>();
+            //builder.RegisterType<EfProductDal>().As<IProductDal>();
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/Contexts/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class DatabaseContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\ProjectsV13; initial catalog=northwind; integrated security=true; Column Encryption Setting = enabled;");
+        }
+
+        //public DbSet<Product> Products { get; set; }
+    }
+}
